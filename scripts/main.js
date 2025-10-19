543f741b9a2ce9d04956dcbb9a533d3f1f81fbeb
danilo <danimoraga1997@gmail.com>
2025-10-19 20:11:27 +0200

    Add typography styles

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
+// Styles
 import '../styles/global.css';
 import '../styles/colors.css';
+import '../styles/typographies.css';
 
+// Node modules
 import { animate } from "animejs";
 
+// Script
 console.log("Hello from main!");
